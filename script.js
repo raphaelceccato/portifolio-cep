24f7ea49d18672c42a43593de27cfb3f2f3e59e9
Raphael <raphaelceccato@hotmail.com>
2023-06-13 10:42:16 -0300

    several changes

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ async function getCEPInfo(cep) {
     }
 
     await fetch(`https://viacep.com.br/ws/${cep}/json/`)
-    .then(response => { return response.json();})
+    .then(response => { return response.json(); })
     .then(data => {
         if (data.erro)
             info.result = "error";
@@ -29,9 +29,10 @@ async function showCEP(cep) {
     let p = document.getElementById("resultado");
 
     let info = await getCEPInfo(cep);
+    p.classList.add("border", "border-muted", "bg-white", "rounded-2", "p-3");
     if (info.result == "ok") {
-        p.innerText = `CEP: ${info.data.cep}\nLogradouro: ${info.data.logradouro}\nBairro:${info.data.bairro}\nCidade:${info.data.localidade}\nEstado:${info.data.uf}`;
+        p.innerHTML = `<b>CEP:</b> ${info.data.cep}<br><b>Logradouro:</b> ${info.data.logradouro}<br><b>Bairro:</b> ${info.data.bairro}<br><b>Cidade:</b> ${info.data.localidade}<br><b>Estado:</b> ${info.data.uf}`;
     } else if (info.result == "error") {
-        p.innerText = "Erro: " + (info.error ?? "API retornou erro");
+        p.innerHTML = "<b>Erro:</b> " + (info.error ?? "API retornou erro");
     }
 }
